33cffbd30e7d26a37e219628e6c15d5ed0896ca4
Àîýƽ <liuypf@yutong.com>
2018-01-03 15:14:32 +0800

    上传处理修改

--- routes/upload.js
+++ routes/upload.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 var upload = require('./fileuploads.js')
 
-/* GET users listing. */
+/* 文件上传请求 */
 router.post('/', upload.single('avatar'), function(req, res, next) {
 	if (req.file) {
-        res.send('文件上传成功')
-        console.log(req.file);
-        console.log(req.body);
+        // res.send('文件上传成功')
+        res.json({ 
+        	code: 0,
+        	msg: '文件上传成功',
+        	data: req.file
+        });
     }
 });
 
